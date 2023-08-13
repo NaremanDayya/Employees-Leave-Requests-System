4e1faa3adec7e52fbd65c57465d569517d4f3e17
Nareman <nanadaya166@gmail.com>
2023-08-13 13:01:58 -0700

    13-8-2023

--- resources/views/employee/notifications.blade.php
+++ resources/views/employee/notifications.blade.php
@@ -1,16 +1,28 @@
 <x-layout>
     <div class="container mt-5">
         <strong class="me-auto">Notifications</strong>
-        @foreach ($notifications as $notification )
-        <div class="alert {{ str_contains($notification->content, 'Accepted') ? 'alert-success' :
-         (str_contains($notification->content, 'Rejected') ? 'alert-danger' : 'alert-info') }}"
-          role="alert">
-            {{ $notification->content }}
+        @foreach ($notifications as $notification)
+        <div class="row">
+            <div class="col">
+                <div class="alert {{ str_contains($notification->content, 'Accepted')
+                    ? 'alert-success'
+                    : (str_contains($notification->content, 'Rejected')
+                        ? 'alert-danger'
+                        : 'alert-info') }}"
+                    role="alert">
+                    {{ $notification->content }}
+                    <span class="float-end">{{ $notification->created_at->diffForHumans(null, true, true) }}</span>
+                </div>
+            </div>
         </div>
-        <small>{{ $notification->created_at->diffForHumans(null, true, true) }}</small>
-
-        @endforeach
-          
         
-</div>
+        
+        
+        
+        
+        
+        @endforeach
+
+
+    </div>
 </x-layout>
